c5ad688df35eab84413795c89f2cc8ff3f6bb12a
briankanya <briankanya@gmail.com>
2017-10-24 09:33:12 -0400

    Add files via upload

---
+++ dragon/app.js
@@ -0,0 +1,25 @@
+var settings = {
+	width: 800,
+	height: 600,
+	backgroundColor: 0x1099bb
+};
+
+var app = new PIXI.Application(settings);
+
+$("body").append(app.view);
+
+var dragon = PIXI.Sprite.fromImage('./dragon.png');
+dragon.anchor.set(0.5);
+dragon.x = app.renderer.width - app.renderer.width / 10;
+dragon.y = app.renderer.height / 2;
+
+var hero = PIXI.Sprite.fromImage('./hero.png');
+hero.anchor.set(0.5);
+hero.x = app.renderer.width - app.renderer.width / 1.5;
+hero.y = app.renderer.height / 2;
+
+app.stage.addChild(dragon, hero);
+
+app.ticker.add(function(delta) {
+	dragon.rotation += 0.1 * delta;
+});
